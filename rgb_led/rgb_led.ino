bba4b4fca560b06831e2a0e13e9853bb84cf123b
Sebastien Lemieux <sebas.lemieux@gmail.com>
2018-02-24 11:47:16 -0500

    color test

--- rgb_led/rgb_led.ino
+++ rgb_led/rgb_led.ino
@@ -20,25 +20,20 @@ void loop()
 {
   setColor(255, 0, 0);  // red
   delay(1000);
-  setColor(0, 255, 0);  // green
-  delay(1000);
-  setColor(0, 0, 255);  // blue
-  delay(1000);
-  setColor(255, 255, 0);  // yellow
+  setColor(255, 32, 0);  // yellow
   delay(1000);  
-  setColor(80, 0, 80);  // purple
+  setColor(0, 64, 0);  // green
   delay(1000);
-  setColor(0, 255, 255);  // aqua
-  delay(1000);
+//  setColor(0, 0, 255);  // blue
+//  delay(1000);
+//  setColor(80, 0, 80);  // purple
+//  delay(1000);
+//  setColor(0, 255, 255);  // aqua
+//  delay(1000);
 }
 
 void setColor(int red, int green, int blue)
 {
-  #ifdef COMMON_ANODE
-    red = 255 - red;
-    green = 255 - green;
-    blue = 255 - blue;
-  #endif
   analogWrite(redPin, red);
   analogWrite(greenPin, green);
   analogWrite(bluePin, blue);  
