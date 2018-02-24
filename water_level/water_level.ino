6aae0d5db0a525a454ea955369cea8c13c64b38b
Sebastien Lemieux <sebas.lemieux@gmail.com>
2018-02-24 11:47:23 -0500

    it works!

--- water_level/water_level.ino
+++ water_level/water_level.ino
@@ -1,9 +1,13 @@
 const int red_pin = 10;
 const int green_pin = 9;
 const int blue_pin = 8;
-const int water_pin = 7;
+const int water_pin = A0;
 
-int level = 0;
+const int sensor_max = 600;
+const int sensor_min = 480;
+int sensor_diff = 0;
+
+float level = 0;
 int red = 0;
 int green = 0;
 void setup() {
@@ -11,23 +15,32 @@ void setup() {
   pinMode(red_pin, OUTPUT);
   pinMode(green_pin, OUTPUT);
   pinMode(blue_pin, OUTPUT);
+  sensor_diff = sensor_max - sensor_min;
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   level = analogRead(A0);
-  Serial.println(level);
-  level = level - 500;
+  Serial.print("level : ");
+  Serial.print(level);
+  Serial.print(" diff : ");
+  Serial.print(sensor_diff);
+  Serial.print(" after : ");
+  level = level - sensor_min;
   if(level < 0) level = 0;
-  else if(level > 60) level = 60;
-  level = level / 60 * 100;
+  else if(level > sensor_diff) level = sensor_diff;
+  level = level / sensor_diff * 100;
+  Serial.println(level);
   red = 255 - ((level - 50) * 5.1);
   if (red > 255) red = 255;
-  green = level * 5.1;
-  if (green > 255) green = 255;
+  green = level * 64 / 100;
+  Serial.print("red   : "); 
+  Serial.println(red);
+  Serial.print("green : ");
+  Serial.println(green);
   setColor(red,green,0);
-  delay(10);
+  delay(1000);
 }
 
 
